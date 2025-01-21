070f076aebd69d7452d85a8bbf32b02ed53616c5
kishan saini <kishansaini666@gmail.com>
2025-01-21 16:44:21 +0530

    first commit

---
+++ conditional.oy.py
@@ -0,0 +1,31 @@
+
+age=10
+if(age >=18):
+    print("vote")
+elif(age < 18):
+    print("underage")
+else:
+    statmentN
+
+# python is indentation language
+# traffic light code
+# light=input("lightcolor : ")
+# if(light == "red"):
+#     print("stop")
+# elif(light == "yellow"):
+#     print("loop")
+# elif(light == "green"):
+#     print("go")
+# else:
+#     print("light is broken")
+
+    # marks
+    marks = int(input("enter marks: "))
+if (marks >= 80):
+        print("A")
+elif (marks >= 80 and marks < 90):
+    print("B")
+elif(marks >= 70 and marks < 60):
+    print("c")
+else:
+    print("fail")
