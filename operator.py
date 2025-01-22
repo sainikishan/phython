513e8ee5d501269e7708d7d0bacb951f9e3cd21c
kishan saini <kishansaini666@gmail.com>
2025-01-22 16:18:27 +0530

    logical operators

---
+++ operator.py
@@ -0,0 +1,42 @@
+# arithmetic operators
+a = 5
+c = 4
+sum = a + c
+print(a + c)
+print(a - c)
+print(a * c)
+print(a % c)
+print(a % c) #%=> means find remainder
+print(a ** c) #**means power
+
+# comparison operator relational operators
+# ==, !=, >, <, >=, <=
+a=50
+b=20
+print(a==b) #checks if a is equal to b
+print( a != b)
+print(a>=b) #grater than 
+print(a<=b) #less than or equal to
+
+# Assigment opearors
+# =, +=, -=, *=, /=, %=, **=, //=, <<=,
+num=10
+# num = num+10
+num +=10
+num %=5
+print("num : ",num) #prints 20
+# Logical operators work on the boolean
+# and, or, not
+a=50
+b=20
+print(not False)
+print(not (a > b))
+# and
+val1= True
+val2= False
+print("and opeartor", val1 or val2)
+# or oerator
+val1= True
+val2= False
+print("and opeartor", val1 or val2)
+print("Or opeartor", val1 or val2)
