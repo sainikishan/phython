c4decbd6e37ef6b4e31ab04ff7b31733cea76b3a
kishan saini <kishansaini666@gmail.com>
2025-01-22 17:22:03 +0530

    logical operators

---
+++ inputin.py
@@ -0,0 +1,31 @@
+# use input statment
+# val=input("enter your name:")
+# print("welcome:",val)
+# print(type(val),val)
+
+# name = input("enter name:")
+# age = input("enter age:")
+# marks = input("enter marks:")
+# print("name:",name)
+# print("age:",age)
+# print("marks:",marks)
+
+# add two nuber
+first=int(input("enter first : "))
+second=int(input("enter second : "))
+print("sum =",float(first+second))
+
+# print area
+side=float(input("enter square side : "))
+print("area =", side*side)
+
+# average value print
+first=int(input("enter first : "))
+second=int(input("enter second : "))
+print("sum =",(first+second/2))
+
+#a>=b print true,false
+first=int(input("enter first : "))
+second=int(input("enter second : "))
+print(first>=second)
+
---
+++ typeconvertsion.py
@@ -0,0 +1,11 @@
+# type conversytion
+a  = 2
+b = 4.25
+sum = a + b
+print(sum)
+
+# typecasting
+a  =int("2")
+b = 4.25
+print(type(a))
+print(a + b)
