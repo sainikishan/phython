20ad29dc4e2895caebbd3edfdd567cc579f068ad
kishan saini <kishansaini666@gmail.com>
2025-01-23 16:10:41 +0530

    logical operators

---
+++ ch2string.py
@@ -0,0 +1,12 @@
+#string is data type 
+str1="this is a string.\n we are cretaing "
+print(str1)
+#operation perform with diferent words (+)
+str2="kishan"
+str3="saini"
+finalstring=str2+ " "+str3
+print(len(finalstring))
+print(finalstring)
+# lenght of str find(len)
+str2="kishan"
+print(len(str2))
