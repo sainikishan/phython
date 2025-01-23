fcf5348eefa8b0b58bcc0a28767cee827726166c
kishan saini <kishansaini666@gmail.com>
2025-01-23 16:27:27 +0530

    logical operators

---
+++ ch2string2.py
@@ -0,0 +1,8 @@
+str="i am study py from kishan"
+print(str.endswith("bh"))
+# caitalize
+str="i am study py from kishan"
+print(str.capitalize())
+# replce old to new
+str="i am study py from kishan"
+print(str.replace("py","java"))
