926151d99d5aaca2d89751b2af5317c2df794384
kishan saini <kishansaini666@gmail.com>
2025-01-23 16:31:07 +0530

    string all

--- ch2string2.py
+++ ch2string2.py
@@ -6,3 +6,9 @@ print(str.capitalize())
 # replce old to new
 str="i am study py from kishan"
 print(str.replace("py","java"))
+# find
+str="i am study py from kishan"
+print(str.find("py"))
+# count 
+str="i am study py from kishan"
+print(str.count("y"))
