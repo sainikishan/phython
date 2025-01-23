ce81571886d04c437da8e7c7f8d00167920d7e14
kishan saini <kishansaini666@gmail.com>
2025-01-23 16:22:33 +0530

    logical operators

--- ch2string.py
+++ ch2string.py
@@ -7,6 +7,16 @@ str3="saini"
 finalstring=str2+ " "+str3
 print(len(finalstring))
 print(finalstring)
-# lenght of str find(len)
+# lenght of str find(len) space are also count
 str2="kishan"
 print(len(str2))
+# indexing start is always 0
+str2="kishan s"
+print(str2[6])
+# slicing accessing parts in a string 
+str2="kishan saini"
+print(str2[1:5]) # 0 to 5 index
+# slice in negative index
+str3="apple"
+print(str3[-1]) # last index
+
