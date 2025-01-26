166254d39cb262b50e61e87830ba12e2f24be8c9
kishan saini <kishansaini666@gmail.com>
2025-01-26 11:39:02 +0530

    new update code hrm

---
+++ nestestloop.py
@@ -0,0 +1,32 @@
+age = 95
+
+if age >= 18:
+    if age >= 80:
+        print("You are an old man")
+    else:
+        print("You are an adult")
+else:
+    print("Cannot drive")
+#ood and even progremm
+num=int(input("enter number:"))
+if num%2==0:
+    print("Even number")
+else:
+   print("Odd number")
+# find greather number find
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+c=int(input("enter thirdnumber:"))
+if (a>=b and a>=c):
+    print("first is greater than",a)
+elif(b >=c):
+    print("is second greater than",b)
+else:
+    print("is third greater than",c)
+
+# number is a multiple 7 or not
+num=int(input("enter number:"))
+if num%7==0:
+    print("number is multiple of 7")
+else:
+    print("number is not multiple of 7")
