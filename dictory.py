3d749364072cc40cad569e399f34b73b35234282
kishan saini <kishansaini666@gmail.com>
2025-01-26 12:47:13 +0530

    new update code hrm

---
+++ dictory.py
@@ -0,0 +1,16 @@
+dict={
+    "name" :"kishan",
+    "cgpa":6.0,
+    "marks":[90,97,87],
+}
+print(dict)
+info={
+    "name" :"kishan",
+    "cgpa":6.0,
+    "marks":[90,97,87],
+    "address":"kanpur",
+    "age":27,
+    "mobile":7905057839
+}
+print(info)
+print(info["name"])
