d091453d6d9cddeb9459dfc3cdaa45f383e80ce8
kishan saini <kishansaini666@gmail.com>
2025-01-26 11:55:42 +0530

    list and string

---
+++ chapter2listandtyple.py
@@ -0,0 +1,23 @@
+marks=[10,20.9,20]
+print(marks)
+print(type(marks))
+print(marks[1])
+print(marks[2])
+print(len(marks))
+
+student= ["kishan",80,"delhi",70,40,20]
+student[1]= "90"
+print(student[1:3])
+# slice
+# list method
+list=[2,1,3]
+list.append(4)
+print(list)
+# listsort
+list=[2,1,3]
+list.sort()
+list.sort(reverse=True) #desecending order
+print(list)
+
+# string are inmultiabple in phyton
+# list in multiable
