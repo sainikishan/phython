bb8b35e35ccfa1ba5979ac4305f3fed8c789d270
all uodate <kishansaini666@gmail.com>
2025-02-04 19:44:36 +0530

    talk chnages

--- set.py
+++ set.py
@@ -6,13 +6,17 @@ print(type(collection))
 print(len(collection))
 # create a empty set
 # set method
-# add() method
+# add() method set ke adar sab hash value adte haii
 collection=set()
 collection.add(1)
 collection.add(2)
 collection.add(3)
 collection.add(6)
-collection.remove(6)
-print(collection) 
+collection.add(6)
+# collection.remove(6)
+# print(collection.pop())
+collection1={1,2,3,3,4,5,6}
+collection2={1,2,3,3,4,5,6}
+print(collection.union(collection2)) 
 
 
