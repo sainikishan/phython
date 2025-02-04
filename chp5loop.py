528593048f1463ce29fea301f0712901406eb744
all uodate <kishansaini666@gmail.com>
2025-02-04 20:09:03 +0530

    talk chnages

---
+++ chp5loop.py
@@ -0,0 +1,29 @@
+# while loop are used to repaer instucntion
+# two type of loop foe loop while loop
+
+# count = 1
+# while count <=5 :
+#     print("Hello, World!")
+#     count =count + 1
+   
+# i = 1
+# while i <=5:
+#     print("kishan, World!",i)
+#     i = i + 1
+
+i = 5
+while i > 1:
+    print(i)
+    i -= 1  # Decrement inside the loop
+print("end")
+
+# print numnber 1 to 100
+# i = 1
+# while i<=100:
+#     print(i)
+#     i = i + 1   
+# print number 100 to 1
+i = 100
+while i>=1:
+    print(i)
+    i = i - 1 
