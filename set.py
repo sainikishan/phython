5ae34c8623fc2b1e5dd8a81839124d9560953224
all uodate <kishansaini666@gmail.com>
2025-02-04 19:39:13 +0530

    talk chnages

---
+++ set.py
@@ -0,0 +1,18 @@
+
+#set is unorder values ignore duplicate value
+collection={1,2,3,3,4,5,6}
+print(collection)
+print(type(collection))
+print(len(collection))
+# create a empty set
+# set method
+# add() method
+collection=set()
+collection.add(1)
+collection.add(2)
+collection.add(3)
+collection.add(6)
+collection.remove(6)
+print(collection) 
+
+
