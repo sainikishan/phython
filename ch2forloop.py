e8cb448e45bcc01350e03c7064f4ea111ec68c89
all uodate <kishansaini666@gmail.com>
2025-02-05 17:15:06 +0530

    for loop

---
+++ ch2forloop.py
@@ -0,0 +1,21 @@
+list = [1,2,3,4,5,6]
+for val in list:
+    print(val)
+veggies=["a","b","c","d","f"]
+for val in veggies:
+ print(val)
+
+ str ="kisham siani"
+ for char in str:
+    print(char)
+
+# for loop with else
+ str ="kisham siani"
+ for char in str:
+    if char == "a":
+       print("a found")
+       break
+    print(char)
+    
+else:
+    print("ended")
