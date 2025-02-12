93c87083dda80701d9e4cdcf11624b8233fa08e3
all uodate <kishansaini666@gmail.com>
2025-02-12 19:52:11 +0530

    formloop

--- ch2forloop.py
+++ ch2forloop.py
@@ -29,3 +29,18 @@ else:
        print(i)
 for i in range(1,4,9):
        print(i)
+      #  Practice Question
+# find n number using while naytral
+n = 10
+sum=0
+for i in range(n+1):
+ 
+   sum += i
+   print("total sum is =",sum)
+   # factiroal n natural number
+   n = 10
+   sum = 1
+   for i in range(1,n+1):
+      sum *= i
+      print("factorial of",n,"is =",sum)
+      
