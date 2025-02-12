5b6bd5a596169e424fbf53b1f4eef72be305f216
all uodate <kishansaini666@gmail.com>
2025-02-12 20:06:30 +0530

    function chapter 6

---
+++ functionnewchanpter.py
@@ -0,0 +1,12 @@
+# Function is a block of statement that perform a specfic task
+# Function can take arguments and return values
+# Function can be reused in the program
+# write a function
+# create a dunction start with def
+def sum(a,b):
+    s =a + b
+    s =a - b
+    s =a * b
+    s =a % b
+    return s
+print(sum(2,3))
