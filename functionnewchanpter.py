19b4f914dd8ad9eb95fbf1d8e3c25e7e41e8a0b9
all uodate <kishansaini666@gmail.com>
2025-02-12 20:08:04 +0530

    function chapter 6

--- functionnewchanpter.py
+++ functionnewchanpter.py
@@ -5,8 +5,10 @@
 # create a dunction start with def
 def sum(a,b):
     s =a + b
-    s =a - b
-    s =a * b
-    s =a % b
     return s
 print(sum(2,3))
+# average of 3 number an sum also
+def average(a,b,c):
+    s = a + b + c
+    return s/3
+print(average(2,3,4))
