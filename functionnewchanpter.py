3b7cc2653ad9b47d7574fd5de71876acd58c4aa7
all uodate <kishansaini666@gmail.com>
2025-02-14 19:26:18 +0530

    fprferlist

--- functionnewchanpter.py
+++ functionnewchanpter.py
@@ -19,4 +19,39 @@ print(average(2,3,4))
 def sum(a,b):
     s = a + b
     return s
+    
 print(sum(2,3))
+cit=["a","b","c","f","d"]
+# print(cit[0],end="")
+def print_len(cit):
+    print(len(cit))
+heroes=["ksihan","kuar"]
+def print_list(heroes):
+    for i in heroes:
+        print(i)
+        print_list(heroes) # function with argument and return value
+# fectorial of n
+ # function with argument and return value
+n=5
+fact = 1
+for i in range (1,n+1):
+    fact = fact * i
+    print(fact) # function with argument and return value
+
+    # using function
+    n = 6
+    def cal(n):
+        fact = 1
+        for i in range (1,n+1):
+            fact = fact * i
+            return fact
+        print(fact) # function with argument and return v
+        # write a function usd to inr
+        # function with argument and return value
+        def converter(usd_val):
+          inr_val = usd_val * 83  # Assuming 1 USD = 83 INR
+    print(f"usd_val USD = inr_val INR")
+
+# Taking user input
+usd_value = float(input("Enter amount in USD: "))
+converter(usd_value)
