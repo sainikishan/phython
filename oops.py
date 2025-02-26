05882a1ed4aca0490364801d967bde7ecdd2e7bf
all uodate <kishansaini666@gmail.com>
2025-02-26 15:55:04 +0530

    calss in py

---
+++ oops.py
@@ -0,0 +1,20 @@
+# oop is python
+# to map with real world scenarios,we started using object in code.ConnectionAbortedError
+# this is called oop
+# oop is python
+#  redenacy redurceuse reusabilty are increaes
+#class is a blueprint for creating object
+class Student:
+    name="kishan"
+    age=20
+# crreatimg a object
+s1=Student()
+print(s1.name)
+print(s1.age)
+print(s1)
+
+# facotry
+class Car:
+    color= "blue"
+car1=Car()
+print(car1.color)
