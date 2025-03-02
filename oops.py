9619d15929a8c4fe20e02a986fb7ed714723a887
all uodate <kishansaini666@gmail.com>
2025-03-02 16:54:39 +0530

    update dashbaord

--- oops.py
+++ oops.py
@@ -4,17 +4,33 @@
 # oop is python
 #  redenacy redurceuse reusabilty are increaes
 #class is a blueprint for creating object
-class Student:
-    name="kishan"
-    age=20
-# crreatimg a object
-s1=Student()
-print(s1.name)
-print(s1.age)
-print(s1)
+# class Student:
+#     name="kishan"
+#     age=20
+# # crreatimg a object
+# s1=Student()
+# print(s1.name)
+# print(s1.age)
+# print(s1)
 
 # facotry
-class Car:
-    color= "blue"
-car1=Car()
-print(car1.color)
+# class Car:
+#     color= "blue"
+# car1=Car()
+# print(car1.color)
+
+
+# constructor
+# all calsses have a function called init(),whick is always excuted when the class is beigninitated.
+
+        
+
+class Student:
+    def __init__(self, fullname):
+        self.name = fullname
+        print("This is a constructor")
+
+# Creating an object outside the class
+s1 = Student("kishan kishan")
+print(s1.name)
+
